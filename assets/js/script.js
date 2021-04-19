525136eb202e583fc601714a3519f61a12eada6d
jasonmueller <jmuel9999@gmail.com>
2021-04-19 18:21:53 -0500

    working on secondary weather fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,22 @@ const searchBtn = document.getElementById(`searchBtn`)
 
 // Fetch function for openweathermap API
 const getCityWeather = function() {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInput.value}&limit=2&appid=${openWeatherApiKey}`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInput.value}&limit=1&appid=${openWeatherApiKey}`)
         .then(response => response.json())
         .then((data) => {
-            console.log(data, "it works");
+            //console.log(data, "it works");
+            // Get the lat and long for selectedCity
             let geoDataLat = data[0].lat;
             let geoDataLon = data[0].lon;
-            console.log(geoDataLat, geoDataLon);
+            //console.log(geoDataLat, geoDataLon);
+            fetch(`api.openweathermap.org/data/2.5/weather?lat=${geoDataLat}&lon=${geoDataLon}&limit=1&appid=${openWeatherApiKey}`)
+            console.log(fetch);
+            // .then((response => response.json))
+            // .then ((detailsData) => {
+            //     Grab weather data from the city searched
+            //     let temperature = detailsData[1].temp
+            //     console.log(temperature);
+            // })
         })
         //console.log("works");
     // Weather fetch
